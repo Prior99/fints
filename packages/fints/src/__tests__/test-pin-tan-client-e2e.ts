7961a04470918393a8c352ed974b0839a850f07b
Mathias Arens <mathias@arens-festers.de>
2019-12-19 22:52:16 +0000

    Latest changes

--- packages/fints/src/__tests__/test-pin-tan-client-e2e.ts
+++ packages/fints/src/__tests__/test-pin-tan-client-e2e.ts
@@ -1,25 +1,66 @@
-import { format } from "date-fns";
-import { readFileSync } from "fs";
-import * as fetchMock from "fetch-mock";
+import * as fs from "fs";
+import { Dialog, DialogConfig } from "../dialog";
+import { Parse } from "../parse";
 import { PinTanClient } from "../pin-tan-client";
-import { encodeBase64, decodeBase64 } from "../utils";
-import { Format } from "../format";
+import { Response } from "../response";
+import { HISPA } from "../segments/hispa";
+import { HITAN } from "../segments/hitan";
+import { HKSPA } from "../segments/hkspa";
+import { HKTAN } from "../segments/hktan";
+import { SEPAAccount } from "../types";
+import { TanRequiredError } from "../errors/tan-required-error";
 
-const url = "https://fints.comdirect.com/fints";
-const name = "bla";
-const pin = "bla";
-const blz = "bla";
-const productId = "bla";
+const url = process.env.FINTS_URL;
+const name = process.env.FINTS_USER;
+const pin = process.env.FINTS_PASSWORD;
+const blz = process.env.FINTS_BLZ;
 
-test("accounts", async () => {
-    const startDate = new Date("2018-08-10T12:00:00Z");
-    const endDate = new Date("2018-10-10T12:00:00Z");
+// const url = process.env.COMDIRECT_FINTS_URL;
+// const name = process.env.COMDIRECT_FINTS_USER;
+// const pin = process.env.COMDIRECT_FINTS_PASSWORD;
+// const blz = process.env.COMDIRECT_FINTS_BLZ;
 
-    const client = new PinTanClient({ blz, name, pin, url, productId });
+// const url = process.env.ING_DIBA_FINTS_URL;
+// const name = process.env.ING_DIBA_FINTS_USER;
+// const pin = process.env.ING_DIBA_FINTS_PASSWORD;
+// const blz = process.env.ING_DIBA_FINTS_BLZ;
 
-    const accounts = await client.accounts();
-    console.info(accounts); // List of all accounts.
+const productId = "9FA6681DEC0CF3046BFC2F8A6";
 
-    const statements = await client.statements(accounts[0], startDate, endDate);
-    console.info(statements); // List of all statements with transactions in specified date range.
-});
+test.skip("get accounts", async () => {
+    const client = new PinTanClient({ blz, name, pin, url, productId, debug: true });
+    try {
+        const accounts = await client.accounts();
+        console.info(accounts); // List of all accounts
+        await fs.writeFileSync('/tmp/account.json', JSON.stringify(accounts[0]))
+    } catch (error) {
+        if (error instanceof TanRequiredError) {
+            const hitan = error.hitan;
+            fs.writeFileSync('/tmp/hitan-auftragsreferenz.txt', hitan.transactionReference);
+            fs.writeFileSync('/tmp/challenge.png', hitan.challengeMedia);
+        } else {
+            console.error(error);
+        }
+    }
+}, 600000);
+
+test.skip("get statements", async () => {
+    const client = new PinTanClient({ blz, name, pin, url, productId, debug: true });
+    const account: SEPAAccount = JSON.parse((await fs.readFileSync('/tmp/account.json') as Buffer).toString());
+
+    const startDate = new Date("2019-11-01T12:00:00Z");
+    const endDate = new Date("2019-12-09T12:00:00Z");
+
+    try {
+        const statements = await client.statements(account, startDate, endDate);
+        console.info(statements);
+    } catch (error) {
+        if (error instanceof TanRequiredError) {
+            const hitan = error.hitan;
+            await fs.writeFileSync('/tmp/hitan-auftragsreferenz.txt', hitan.transactionReference);
+            await fs.writeFileSync('/tmp/challenge.png', hitan.challengeMedia);
+        } else {
+            console.error(error);
+        }
+    }
+}, 600000);
